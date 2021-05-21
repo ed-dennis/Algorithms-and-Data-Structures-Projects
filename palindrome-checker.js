7cba21abde38a547a87409aae0139c4316c91cc2
Ed Dennis <82810267+ed-dennis@users.noreply.github.com>
2021-05-21 12:40:44 +0100

    Create palindrome-checker.js

---
+++ palindrome-checker.js
@@ -0,0 +1,10 @@
+function palindrome(str) {
+
+  const newStr = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+  const revStr = newStr.split('').reverse().join('');
+  if (newStr === revStr) {
+    return true;
+   } else {
+     return false;
+   }
+}
